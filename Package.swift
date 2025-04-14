d683fdcdcecb362ab4d68f9eee22ac2828becf91
acapadev <antonio.caparello@digitouch.it>
2025-04-14 15:03:23 +0200

    chore: pin dependencies

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["CieSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.25.2"),
-        .package(url: "https://github.com/apple/swift-asn1.git", from: "1.3.1"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", revision: "333f51104b75d1a5b94cb3b99e4c58a3b442c9f7"), // -> "1.25.2"
+        .package(url: "https://github.com/apple/swift-asn1.git", revision: "ae33e5941bb88d88538d0a6b19ca0b01e6c76dcf"), // -> "1.3.1"
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
